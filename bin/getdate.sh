67f9a154a15490055620dd242714b3bd3bdf5885
Bai Liang <bailiangcn@gmail.com>
2011-03-18 12:10:06 +0800

    增加getdata.sh,解决星期数字的问题

---
+++ bin/getdate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  getdate.sh
+# 
+#         USAGE:  ./getdate.sh 
+# 
+#   DESCRIPTION:  返回时间,用于xmobar菜单条使用
+#        AUTHOR:  BaiLiang , bailiangcn@gmail.com
+#       COMPANY:  DQYTV
+#       VERSION:  1.0
+#       CREATED:  2011-03-18 11:28:19
+#===============================================================================
+
+echo `date +%Y年%m月%d日\ %H:%M:%S\ %A`
