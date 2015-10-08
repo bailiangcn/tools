c4e562b50fc9365477096cce151c71c81192b9a4
bl_home <bailiangcn@gmail.com>
2015-10-08 22:16:36 +0800

    调整了配置，备份

--- bin/pyton_skeleton/test.py
+++ bin/pyton_skeleton/test.py
@@ -1,8 +1,11 @@
 import sys
 import os
 
-sys.path.append(os.curdir)
-sys.path.append(os.path.join(os.pardir, ''))
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+PAR_DIR = os.path.abspath(os.path.join(CUR_DIR, os.path.pardir))
+
+sys.path.append(os.path.abspath(PAR_DIR))
+sys.path.append(os.path.abspath(CUR_DIR))
 
 from unittest import TestCase
 
