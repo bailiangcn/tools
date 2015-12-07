3f2cc8afe5038f2534d1b76d0972fb5ba9638f2c
bl_home <bailiangcn@gmail.com>
2015-12-07 21:24:33 +0800

    更改为通达信版本

--- bin/download.bat
+++ bin/download.bat
@@ -2,67 +2,24 @@
 
 REM
 REM 本脚本负责把云盘的自选股、日记下载到通达信软件
-REM 需要安装 winrar
 REM
 
-set homename=BL-STOCK
-set officename=bailiang-stock
-
-REM 备份原有自选股
-rem 格式化日期
-rem date出来的日期是"2006-02-22 星期三"，不能直接拿来使用，所以应该先格式化一下
-rem 变成我们想要的。date:~0,4的意思是从0开始截取4个字符
-set d=%date:~0,4%%date:~5,2%%date:~8,2%
-rem 设定压缩程序路径，这里用的是WINRAR的rar.exe进行打包的
-set path=C:\Program Files\WinRAR;c:\windows\system32
-
-
 rem 设定要替换的文件名
-set srcFile=自选股*.BLK
-rem
-rem 设定备份文件所在目录
-set dstDir=C:\temp
-rem 设定备份文件名称前缀
-set dstFile=backup_自选_
-rem 设定备份文件的前缀,目前为temp,前缀为backup
-set webPrefix=
+set srcFile1=blocknew
+set srcFile2=diary
 
-set srcDir=
+rem 设定要通达信数据目录
+set srcDir=C:\new_haitong\T0002
+rem 设定网盘的目录
+set netDir=C:\云U盘\kp_bailiangcn@gmail.com\stock
 
-if "%computername%"=="%homename%" (
-    echo your computer is at home.
-        rem 设定要备份的目录
-        rem set srcDir=C:\Program Files\dzh2\USERDATA
-	set srcDir=C:\dzh365\USERDATA
-        rem 设定网盘的目录
-        set netDir=Z:\stock
-    ) 
-if "%computername%"=="%officename%" (
-    echo your computer is at office.
-    rem 设定要备份的目录
-    rem set srcDir=C:\dzh365\USERDATA
-    set srcDir=C:\Program Files\dzh2\USERDATA
-    rem 设定网盘的目录
-    set netDir=Y:
-    )
-
-if "%srcDir%"=="" (
-    echo Where are you?
-) else (
-    rem 如果备份文件夹不存在就建立文件夹
-    if not exist %dstDir% start mkdir %dstDir%
-    rem 如果文件不存在,开始备份
-    if not exist %dstDir%\%dstFile%%webPrefix%%d%.rar start Rar a %dstDir%\%dstFile%%webPrefix%%d%.rar %srcDir%\block\%srcFile%
     
-    REM 拷贝网盘内的文件
-    echo ******************** 
-    echo 从云盘覆盖本地自选股
-    echo ********************
-    xcopy  /Y "%netDir%\%srcFile%" "%srcDir%\block\"
+REM 拷贝网盘内的文件
+echo ******************** 
+echo 从云盘覆盖本地自选股
+echo ********************
 
-    xcopy  /Y  "%netDir%\MEMO.DAT" "%srcDir%\MEMO.DAT"
-    xcopy /E /Y "%netDir%\NOTE\*.*" "%srcDir%\NOTE\"
-)
+xcopy /I /E /Y "%netDir%\%srcFile1%" "%srcDir%\%srcFile1%"
+xcopy /I /E /Y "%netDir%\%srcFile2%" "%srcDir%\%srcFile2%"
 
-REM pause
 @echo on 
--- bin/upload.bat
+++ bin/upload.bat
@@ -4,36 +4,22 @@ REM
 REM 本脚本负责把海通通达信自选股和日记上传到云盘中
 REM
 
-set homename=BL-STOCK
-set officename=bailiang-stock
 
 rem 设定要替换的文件名
 set srcFile1=blocknew
 set srcFile2=diary
 
-set srcDir=
-
-if "%computername%"=="%homename%" (
-    echo your computer is at home.
-    ) 
-if "%computername%"=="%officename%" (
-    echo your computer is at office.
-    )
 
 rem 设定要备份的目录
 set srcDir=C:\new_haitong\T0002
 rem 设定网盘的目录
 set netDir=C:\云U盘\kp_bailiangcn@gmail.com\stock
 
-if "%srcDir%"=="" (
-    echo Where are you?
-) else (
-    echo ******************** 
-    echo 上传自选股
-    echo ********************
-    xcopy /I /E /Y "%srcDir%\%srcFile1%" "%netDir%\%srcFile1%"
-    xcopy /I /E /Y "%srcDir%\%srcFile2%" "%netDir%\%srcFile2%"
-)
+echo ******************** 
+echo 上传自选股
+echo ********************
+xcopy /I /E /Y "%srcDir%\%srcFile1%" "%netDir%\%srcFile1%"
+xcopy /I /E /Y "%srcDir%\%srcFile2%" "%netDir%\%srcFile2%"
 
 REM pause
 @echo on 
